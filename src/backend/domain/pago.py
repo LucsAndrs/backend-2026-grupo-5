0cbc679c1e371f136a51a308663fe5a8387bdbf6
nikitsz <ncalderon2026@alu.uct.cl>
2026-09-04 21:44:37 -0400

    Agrega clase Pago y EstadoPago con atributos en domain

---
+++ src/backend/domain/pago.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from enum import Enum
+from datetime import date
+
+class EstadoPago(str, Enum):
+    PENDIENTE = "pendiente"
+    EXITOSO = "exitoso"
+    RECHAZADO = "rechazado"
+
+class Pago(BaseModel):
+    id_pago: str
+    id_venta: str
+    fecha_pago: date
+    monto: float
+    metodo_pago: str
+    estado_pago: EstadoPago
