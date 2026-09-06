4479347a50acd1e13880ce8553f7e4cec941d14e
Benjamin-Alarcon <balarcon2026@alu.uct.cl>
2026-09-06 17:43:55 -0300

    feat: agrega endpoint POST /categorias y separa Categoria en CategoriaCreate
    Se separa el modelo Categoria en CategoriaCreate para recibir solo los datos de entrada del usuario. Se añadio el metodo crear_categoria con el POST /categorias en main.py que genera automaticamente el id_categoria con uuid.

--- src/backend/domain/categoria.py
+++ src/backend/domain/categoria.py
@@ -1,9 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import date
+import uuid
 
-class Categoria(BaseModel):
-    id_categoria : str
+class CategoriaCreate(BaseModel):
     nombre_categoria : str
     descripcion : str
     fecha_creacion : date
     activa : bool
+    
+class Categoria(CategoriaCreate):
+    id_categoria : str = Field(default_factory=lambda: str(uuid.uuid4()))
--- src/backend/main.py
+++ src/backend/main.py
@@ -1,3 +1,11 @@
 from fastapi import FastAPI, HTTPException
+from .domain.categoria import Categoria, CategoriaCreate
+from .services.categorias_service import (
+    crear_categoria,
+    obtener_categoria)
 
 app = FastAPI()
+
+@app.post("/categorias", response_model=Categoria)
+def crear_cat(categoria: CategoriaCreate):
+    return crear_categoria(categoria)
