2c29c6dba0a01d9a693bb2f6e2f8d6eb0c4a250e
ecayuqueo <cayuqueoelias@gmail.com>
2026-09-08 17:30:01 -0300

    feat: agregar metodos agregar_detalle y calcular-total a la clase venta

--- src/backend/domain/venta.py
+++ src/backend/domain/venta.py
@@ -1,8 +1,24 @@
-from pydantic import BaseModel
+from dataclasses import dataclass, field
 from datetime import date
 
-class Venta(BaseModel):
-    id_venta: str
+from backend.domain.detalleventa import DetalleVenta
+
+@dataclass
+class Venta:
+    id: int
     fecha_venta: date
-    total: float
     cliente: str
+    total: 0.0
+    detalles: list[DetalleVenta] = field(default_factory=list)
+
+    def agregar_detalle(self, detalle: DetalleVenta):
+        if detalle.cantidad_producto <= 0:
+            raise ValueError("La cantidad de producto debe ser mayor a cero")
+        self.detalles.append(detalle)
+        self.total = self.calcular_total()
+
+    def calcular_total(self):
+        total = 0
+        for detalle in self.detalles:
+            total = total + detalle.subtotal
+        return total
