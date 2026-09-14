a15b0d03002f4e2965ad08006245b77bb62253d2
Lucas Villegas <lvillegas2024@alu.uct.cl>
2026-09-14 20:11:33 -0300

    fix: agregar id_venta al dominio para respetar UML

--- src/backend/domain/detalleventa.py
+++ src/backend/domain/detalleventa.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass
 class DetalleVenta:
     id_detalle:  str
     id_producto: str
+    id_venta: str
     cantidad_producto:  int
     subtotal: int
     precio_unitario: int
